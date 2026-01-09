90051cbfbda1e34fa84a15ee5c36ab0fd74f9b9f
Marko <markleler25@gmail.com>
2026-01-09 14:35:02 +0100

    added key input field

--- src/java/controllers/ApiLogic.java
+++ src/java/controllers/ApiLogic.java
@@ -1,5 +1,16 @@
 package controllers;
 
-public class ApiLogic {
+import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.util.TreeMap;
+
+public class ApiLogic {
+    public void returnData(long steam64ID){
+        ObjectMapper mapper = new ObjectMapper();
+        boolean hasMore = true, noNewGames = false;
+        long lastAppId = 0;
+        TreeMap<Integer, String> map = new TreeMap<>(); // used for storing appid : name
+
+        String steamProfileUrl = "http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=7C32F141B780243EECD22CBBDBB61F47&steamids=76561198093875269";
+    }
 }
--- src/java/controllers/LoginUIController.java
+++ src/java/controllers/LoginUIController.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class LoginUIController {
     @FXML
