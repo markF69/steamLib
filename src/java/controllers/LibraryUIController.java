80da1ffae2b4c5e3af5b3183b1c27501c6390fa5
Marko <markleler25@gmail.com>
2026-01-12 19:14:06 +0100

    added collect method

--- src/java/controllers/LibraryUIController.java
+++ src/java/controllers/LibraryUIController.java
@@ -1,4 +1,8 @@
 package controllers;
 
 public class LibraryUIController {
+    // Collects the data from the API class to be used for displaying
+    public static void collectData(){
+        
+    }
 }
