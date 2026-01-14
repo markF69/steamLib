d60a5faf1b4e50a1ae84a47818c8b84e09839120
Marko <markleler25@gmail.com>
2026-01-14 02:10:33 +0100

    almost finished library controller

--- src/java/controllers/LibraryUIController.java
+++ src/java/controllers/LibraryUIController.java
@@ -1,30 +1,61 @@
 package controllers;
 
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.image.ImageView;
 import javafx.scene.text.Text;
+import other.ApiLogic;
+import other.DisplayData;
 import other.Game;
 
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
-public class LibraryUIController {
+
+public class LibraryUIController implements Initializable {
     @FXML
-    private TableColumn nameTC;
+    private TableColumn<Game, String> nameTC;
     @FXML
-    private TableColumn playtimeTC;
+    private TableColumn<Game, String> playtimeTC;
     @FXML
-    private TableColumn lastPlayedTC;
+    private TableColumn<Game, String> lastPlayedTC;
     @FXML
     private ImageView avatarImg;
     @FXML
     private Text nameTxt;
     @FXML
-    private TableView libraryTV;
+    private TableView<Game> libraryTV;
 
-    // Collects the data from the API class to be used for displaying
-    public void collectData(ArrayList<Game> libraryGames, String profileName, String profilePicture, int totalGameCount){
+    // Runs the methods at the start
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        displayData();
+    }
+
+    // Displays data that has been collected from the API class
+    public void displayData(){
+        DisplayData data = ApiLogic.returnData();
+
+        // Assigns the data
+        ArrayList<Game> games = data.getLibraryGames();
+        String profileName = data.getProfileName();
+        String profilePicture = data.getProfilePicture();
+        int gameCount = data.getTotalGameCount();
+
+        // This makes the cells call the getters so that they can display data
+        nameTC.setCellValueFactory(new PropertyValueFactory<>("name"));
+        playtimeTC.setCellValueFactory(new PropertyValueFactory<>("playtimeHours"));
+        lastPlayedTC.setCellValueFactory(new PropertyValueFactory<>("lastPlayedDate"));
+
+        // Puts items
+        libraryTV.setItems(FXCollections.observableArrayList(games));
 
     }
+
+
 }
